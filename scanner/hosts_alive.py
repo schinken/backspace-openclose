983b96b91ba7e19ce0135bcb98a07887f885378b
schinken <schinken@hackerspace-bamberg.de>
2013-06-03 13:32:10 +0200

    using new collectors

--- scanner/hosts_alive.py
+++ scanner/hosts_alive.py
@@ -1,53 +1,49 @@
 #!/usr/bin/python
 
-import commands
-import re
+from collector import ubiquiti, arp, nmap
 
 import MySQLdb
 import MySQLdb.cursors
 
 import iptools
 import settings
-import sys
-
-
-def run_nmap(network):
-
-    cmd = 'nmap -sP -n ' + network
-   
-    (status, output) = commands.getstatusoutput(cmd)
-
-    if status:
-        sys.stderr.write('Error running nmap command')
-        return None
-
-    return output
-
-
-def get_hosts(network):
-
-    output = run_nmap(network)
-    hosts = []
-
-    # parse all mac and ip combinations from string
-    matches = re.findall(r'scan report for\s+((?:\d{1,3}\.){3}\d{1,3}).*?MAC Address:\s+((?:[0-9A-F]{2}\:){5}[0-9A-F]{2})', output, re.M | re.S)
-    for match in matches:
-        hosts.append({'ip': match[0], 'mac': match[1]})
-
-    return hosts
-
-def write_hosts(hosts, db):
-
-    dbcursor = db.cursor()
-
-    # add ALL THE HOSTS to the database
-    for host in hosts:
-        longip = iptools.ip2long(host['ip']) 
-        dbcursor.execute("INSERT INTO alive_hosts (macaddr, iplong, erfda) VALUES ('%s', %d, NOW())" % (host['mac'].lower() , longip))
-
-    dbcursor.close()
 
 def main():
+
+    collectors = []
+
+    # ====== add ubuquiti ======
+    ubnt = settings.ubiquiti
+
+    x = ubiquiti.Collector(settings.network, ubnt['host'], ubnt['user'], ubnt['pass'])
+    x.start()
+
+    collectors.append(x)
+
+    # ====== add arp =======
+    x = arp.Collector(settings.network)
+    x.start()
+
+    collectors.append(x)
+
+    # ====== add nmap ======
+    x = nmap.Collector(settings.network)
+    x.start()
+
+    collectors.append(x)
+
+
+    # waiting for collectors...
+    for collector in collectors:
+        collector.join()
+
+    # results
+    print "The results are in!"
+
+    hosts = {}
+    for collector in collectors:
+        print collector.get_hosts()
+        hosts = dict(hosts.items() + collector.get_hosts().items())
 
     # create database connection
     dbcron = MySQLdb.connect(host=settings.mysql_host,
@@ -55,11 +51,15 @@ def main():
                              passwd=settings.mysql_pass,
                              db=settings.mysql_name)
 
-    # parse host string, returned from nmap
-    hosts  = get_hosts(settings.network)
-
     # write hosts to database
-    write_hosts(hosts, dbcron)
+    dbcursor = dbcron.cursor()
+
+    # add ALL THE HOSTS to the database
+    for ip,mac in hosts.iteritems():
+        longip = iptools.ip2long(ip) 
+        dbcursor.execute("INSERT INTO alive_hosts (macaddr, iplong, erfda) VALUES ('%s', %d, NOW())" % (mac.lower() , longip))
+
+    dbcursor.close()
 
     # close database connection
     dbcron.commit()
