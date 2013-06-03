4cda99bc44f84fec0684c02f271deda161350cfc
schinken <schinken@hackerspace-bamberg.de>
2013-06-03 12:37:57 +0200

    added test collector

---
+++ scanner/testcollect.py
@@ -0,0 +1,46 @@
+from collector import ubiquiti, arp, nmap
+import settings
+
+if __name__ == '__main__':
+    
+    collectors = []
+    net = '10.1.20.100-250'
+
+    # ====== add ubuquiti ======
+    ubnt = settings.ubiquiti
+
+    x = ubiquiti.Collector(net, ubnt['host'], ubnt['user'], ubnt['pass'])
+    x.start()
+
+    collectors.append(x)
+
+    # ====== add arp =======
+    x = arp.Collector(net)
+    x.start()
+
+    collectors.append(x)
+
+    # ====== add nmap ======
+    x = nmap.Collector(net)
+    x.start()
+
+    collectors.append(x)
+
+
+    # waiting for collectors...
+    for collector in collectors:
+        collector.join()
+
+    # results
+    print "The results are in!"
+
+    hosts = {}
+    for collector in collectors:
+        hosts = hosts + collector.get_hosts()
+
+    print hosts
+
+
+
+
+    
